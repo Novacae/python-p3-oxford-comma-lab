83214b7b1b16919704d0e6f5201cba96c5d19dc7
aman githinji <amangithinji09@gmail.com>
2023-10-23 16:36:32 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+     if len(elements) == 1 :
+          return elements[0]
+     elif len(elements) == 2: 
+          return " and ".join(elements)
+     else:
+          return ", ".join(elements[:-1]) + ", and " + elements[-1]
+
+
+
+
+     #+ ", and " + (elements[-1])
+  
